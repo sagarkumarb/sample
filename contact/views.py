caa0ad43c14e63dc466306fc54cc4545616ce99e
sagarb <sagar__kumar@hotmail.com>
2015-03-24 04:07:59 +0530

    Contact form added

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from contact.forms import ContactForm
+
 
 # Create your views here.
+def contact(request):
+    template = "contact_us.html"
+    form = ContactForm()
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        print form.is_valid()
+        if form.is_valid():
+            pass
+    return render(request, template, {'form':form})
--- sample/urls.py
+++ sample/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from contact import views
+
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'sample.views.home', name='home'),
@@ -9,5 +11,5 @@ urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),    
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', home, name="home")
+    url(r'^contact/$', views.contact, name="contact"),
 )
